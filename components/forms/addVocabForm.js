fb2126f507ceec7af49db7ac7168073f867054a5
Kyle B <kberry1298@gmail.com>
2023-06-09 20:45:57 -0500

    mvp completed

--- components/forms/addVocabForm.js
+++ components/forms/addVocabForm.js
@@ -1,6 +1,8 @@
 import clearDom from '../../utils/clearDom';
 import renderToDom from '../../utils/renderToDom';
 
+// When clicked create entry, runs this code to clear dom and create a domString that is then rendered to the DOM
+// value="${obj.category || ''}" part ensures that the radio buttons will have the value of obj.category if it exists, or an empty string if it doesn't, allowing the form to pre-select the appropriate radio button if necessary.
 const addVocabForm = (obj = {}) => {
   clearDom();
   const domString = `
@@ -9,20 +11,25 @@ const addVocabForm = (obj = {}) => {
       <label for="title">Word Title</label>
       <input type="text" class="form-control" id="title" aria-describedby="wordTitle" placeholder="Enter Word Title" value="${obj.title || ''}" required>
     </div>
-
+    <style>
+    .form-check {
+      display: inline-block;
+      margin-right: 10px;
+    }
+  </style>
   <div id="subject-check">
     <div class="form-check">
-    <input type="radio" class="form-check-input" id="radio1" name="optradio" value="Javascript" value="${obj.category || ''}" required>Javascript
-    <label class="form-check-label" for="radio1"></label>
-  </div>
-  <div class="form-check">
-    <input type="radio" class="form-check-input" id="radio2" name="optradio" value="HTML" value="${obj.category || ''}" required>HTML
-    <label class="form-check-label" for="radio2"></label>
-  </div>
-  <div class="form-check">
-  <input type="radio" class="form-check-input" id="radio3" name="optradio" value="CSS" value="${obj.category || ''}" required>CSS
-  <label class="form-check-label" for="radio1"></label>
-</div>
+      <input type="radio" class="form-check-input" id="radio1" name="optradio" value="Javascript" value="${obj.category || ''}" required>
+      <label class="form-check-label" for="radio1">Javascript</label>
+    </div>
+    <div class="form-check">
+      <input type="radio" class="form-check-input" id="radio2" name="optradio" value="HTML" value="${obj.category || ''}" required>
+      <label class="form-check-label" for="radio2">HTML</label>
+    </div>
+    <div class="form-check">
+      <input type="radio" class="form-check-input" id="radio3" name="optradio" value="CSS" value="${obj.category || ''}" required>
+      <label class="form-check-label" for="radio3">CSS</label>
+    </div>
   </div>
 
     <div class="form-group">
--- events/domEvents.js
+++ events/domEvents.js
@@ -1,45 +1,51 @@
-import { getVocab, deleteVocab, filterVocab } from '../api/vocabData';
+import {
+  getVocab, deleteVocab, filterVocab, getSingleVocab
+} from '../api/vocabData';
 import addVocabForm from '../components/forms/addVocabForm';
 import showVocab from '../pages/vocab';
 
 const domEvents = (user) => {
   document.querySelector('#navigation').addEventListener('click', (e) => {
+    // When create entry button it will call the addVocabForm for the user to create a vocab term
     if (e.target.id.includes('create-entry')) {
       addVocabForm();
     }
   });
 
   document.querySelector('#main-container').addEventListener('click', (e) => {
+    // EDIT VOCAB WORD EVENT LISTENER
+    if (e.target.id.includes('edit-book-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      getSingleVocab(firebaseKey).then((vocabObj) => addVocabForm(vocabObj));
+      getSingleVocab(firebaseKey).then(addVocabForm);
+    }
+
+    // DELETE VOCAB WORD EVENT LISTENER
     if (e.target.id.includes('delete-vocab')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Are you sure you want to delete this word?')) {
         const [, firebaseKey] = e.target.id.split('--');
 
         deleteVocab(firebaseKey).then(() => {
-          getVocab().then(showVocab);
+          getVocab(user.uid).then(showVocab);
         });
       }
     }
   });
 
+  // FILTER BUTTONS TO FILTER VOCAB WORDS
   document.querySelector('#filter').addEventListener('click', (e) => {
-    if (e.target.id.includes('math-btn')) {
-      filterVocab(user.uid, 'Math').then(showVocab);
+    if (e.target.id.includes('all-btn')) {
+      getVocab(user.uid).then(showVocab);
     }
-  });
-  document.querySelector('#filter').addEventListener('click', (e) => {
-    if (e.target.id.includes('science-btn')) {
-      filterVocab(user.uid, 'Science').then(showVocab);
+    if (e.target.id.includes('javascript-btn')) {
+      filterVocab(user.uid, 'Javascript').then(showVocab);
     }
-  });
-  document.querySelector('#filter').addEventListener('click', (e) => {
-    if (e.target.id.includes('socialstudies-btn')) {
-      filterVocab(user.uid, 'Social Studies').then(showVocab);
+    if (e.target.id.includes('html-btn')) {
+      filterVocab(user.uid, 'HTML').then(showVocab);
     }
-  });
-  document.querySelector('#filter').addEventListener('click', (e) => {
-    if (e.target.id.includes('english-btn')) {
-      filterVocab(user.uid, 'English').then(showVocab);
+    if (e.target.id.includes('css-btn')) {
+      filterVocab(user.uid, 'CSS').then(showVocab);
     }
   });
 };
